ca1b77dcb109537918d77bf356a541ae628641e0
sisindri-admin <sisindrimacha9988@gmail.com>
2026-08-11 06:07:58 +0530

    bakend server port update

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/', (req, res) => {
 });
 
 // Server Listen
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
