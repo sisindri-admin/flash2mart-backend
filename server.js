a972c9512af79151fbe25ce9ff8de25b25e6b4e3
sisindri-admin <sisindrimacha9988@gmail.com>
2026-08-11 06:36:11 +0530

    updated raiway domain

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://sisindri:9988@flash2ma
 // MongoDB Connection
 mongoose.connect(MONGO_URI)
 .then(() => console.log('✅ MongoDB Connected Successfully'))
-.catch((err) => console.error('❌ MongoDB Connection Error:', err));
+.catch((err) => console.error('❌ MongoDB Connection Error pls clear:', err));
 
 // Routes
 const deliveryRoutes = require('./routes/deliveryRoutes');
